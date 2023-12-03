0eeeefd1e70a8c89891c4a4a04c423a25fc08b72
xufangyou61202 <xufangyou61202@sangfor.com>
2023-12-03 18:15:46 +0800

    [UPDATE] tic-tac-toe: update App.js - addon some comments

--- w001-tic-tac-toe/src/App.js
+++ w001-tic-tac-toe/src/App.js
@@ -1,4 +1,7 @@
+// In React, a component is a piece of reusable code that represents a part of a user interface.
+// The default is a JavaScript keyword tells other files using your code that it's the main function in your file.
 export default function Square() {
+  // <button> is a JSX element.
+  // A JSX element is a combination of JavaScript code and HTML tags that describes what you’d like to display.
   return <button className="square">X</button>;
 }
-  
