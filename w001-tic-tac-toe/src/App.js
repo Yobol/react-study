dcdf56e6a0c14d1480c93bf143ba7cb5fa4c5145
xufangyou61202 <xufangyou61202@sangfor.com>
2023-12-03 18:35:42 +0800

    [UPDATE] tic-tac-toe: passing data through props

--- w001-tic-tac-toe/src/App.js
+++ w001-tic-tac-toe/src/App.js
@@ -1,5 +1,12 @@
 // In React, a component is a piece of reusable code that represents a part of a user interface.
+// the Square component can be passed a prop called value.
+function Square({value}) {
+    // To “escape into JavaScript” from JSX, we need curly braces `{}` to wrapper the prop.
+    return <button className="square">{value}</button>
+}
+
 // The default is a JavaScript keyword tells other files using your code that it's the main function in your file.
+// So we can using <App /> to replace <Board />.
 export default function Board() {
   // <button> is a JSX element.
   // A JSX element is a combination of JavaScript code and HTML tags that describes what you’d like to display. 
@@ -7,19 +14,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
